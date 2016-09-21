debebeca5770184dbad03c984979fa2c9639dc3d
nidiairania <nidia@iib.unam.mx>
2016-09-20 19:53:16 -0500

    se agrego strings y dimens

---
+++ m5_NASA-viernes12/NImod5NASA/app/src/main/java/mx/nitrogena/dadm/mod5/nasa/MainActivity.java
@@ -0,0 +1,13 @@
+package mx.nitrogena.dadm.mod5.nasa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ m5_NASA-viernes12/NImod5NASA/app/src/test/java/mx/nitrogena/dadm/mod5/nasa/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package mx.nitrogena.dadm.mod5.nasa;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
